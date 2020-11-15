9ad7c074c7ec1d88ce325ca62cea8f90f953a836
andregoro <34197591+andregoro@users.noreply.github.com>
2020-11-15 17:28:05 -0300

    s

---
+++ js/login.js
@@ -0,0 +1,16 @@
+
+$("document").ready(function(){
+    $("#cor").css("color","yellow");
+
+  if(sessionStorage.getItem("senha")=="1"){
+    $( ".esconder" ).show();
+    sessionStorage.setItem("ver","true");
+  }else{
+    if(document.getElementById("email_address").value=="andre" && document.getElementById("password").value=="1" || sessionStorage.getItem("ver")=="true"){
+      sessionStorage.getItem("senha")="1";
+      alert(document.getElementById("email_address").value);
+    }
+    $( ".esconder" ).hide();
+    sessionStorage.setItem("ver","false");
+  }
+});
