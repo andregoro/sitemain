ba6bd15cc30e9c294c03413276631c1c98e2b017
andregoro <34197591+andregoro@users.noreply.github.com>
2020-11-15 18:17:40 -0300

    s

---
+++ js/conector.js
@@ -0,0 +1,15 @@
+$("document").ready(function(){
+    $("#cor").css("color","yellow");
+if(document.getElementById("email_address").value=="andre" && document.getElementById("password").value=="1" || sessionStorage.getItem("ver")=="true"){
+        sessionStorage.getItem("senha")="1";
+        alert(document.getElementById("email_address").value);
+}
+  if(sessionStorage.getItem("senha")=="1"){
+    $( ".esconder" ).show();
+    sessionStorage.setItem("ver","true");
+  }else{
+
+    $( ".esconder" ).hide();
+    sessionStorage.setItem("ver","false");
+  }
+});
